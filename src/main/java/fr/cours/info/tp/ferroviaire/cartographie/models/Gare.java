6a5d7a32f673e5a7d2ee71f8e46ac62605b1a364
hendzormati <hend.zormati@esprit.tn>
2026-01-12 17:47:42 +0100

    premier commit

--- src/main/java/fr/cours/info/tp/ferroviaire/cartographie/models/Gare.java
+++ src/main/java/fr/cours/info/tp/ferroviaire/cartographie/models/Gare.java
@@ -1,10 +1,21 @@
 package fr.cours.info.tp.ferroviaire.cartographie.models;
+
 /**
  * Représente une gare du réseau ferroviaire.
- * Une gare possède un nom et un nombre de quais.
- *
- * @author Votre Nom
+ * @author hend zormati
  * @version 1.0
  */
 public class Gare {
+    private Integer idGare;
+    private String nomGare;
+    private String ville;
+    private Integer nbQuai;
+
+    /**
+     * Constructeur par défaut de la classe Gare.
+     */
+    public Gare() {
+    }
+
+
 }
